7d6d696cb75b1a55f70a68490b53cde998accdfc
Greg Logan <gregorydlogan@gmail.com>
2017-09-06 15:53:12 -0600

    Updating sync

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Note that we really, really want H and l to preserve any linking!
-rsync -avhSPuKHlc --delete --exclude=*.db --exclude=mini-dinstall --delete-excluded d-install/ root@apt-cacher:/var/www/repo
-#rsync -avhSPuKHlc --delete --exclude=*.db --exclude=mini-dinstall --delete-excluded d-install/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs
+rsync -avhSPuKHlc --delete d-install/ root@repo:/var/www/repo
+#rsync -avhSPuKHlc --delete d-install/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs
