3c4e528789cdf91b3ffd3646969ef35ca391c367
Greg Logan <gregorydlogan@gmail.com>
2019-05-29 13:05:07 -0600

    Updated sync, now only pushes stable and testing

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #Note that we really, really want H and l to preserve any linking!
-rsync -avhSPuKHlc --delete d-install/ root@repo:/var/www/repo
-#rsync -avhSPuKHlc --delete d-install/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs
+rsync -avhSPuKHlc d-install/stable/ root@repo:/var/www/repo/stable
+rsync -avhSPuKHlc d-install/testing/ root@repo:/var/www/repo/testing
+#rsync -avhSPuKHlc d-install/stable/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs/stable
+#rsync -avhSPuKHlc d-install/testing/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs/testing
