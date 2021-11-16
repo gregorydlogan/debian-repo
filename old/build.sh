1fc0e093bc676dd53d4ee12143e61558c8098931
Greg Logan <gregorydlogan@gmail.com>
2021-11-16 11:56:56 -0600

    Old is now out of date (8.x and older), so new is now just current

--- old/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -uxe
-
-mini-dinstall -vbc mini-dinstall.conf
--- old/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Note that we really, really want H and l to preserve any linking!
-rsync -avhSPuKHlc d-install/stable/ root@repo:/var/www/repo/stable
-rsync -avhSPuKHlc d-install/testing/ root@repo:/var/www/repo/testing
-#rsync -avhSPuKHlc d-install/stable/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs/stable
-#rsync -avhSPuKHlc d-install/testing/ greglogan@pkg.opencast.org:/srv/www/pkg.opencast.org/debs/testing
